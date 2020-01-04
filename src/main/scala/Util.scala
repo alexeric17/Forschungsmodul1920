36fa2c98cfaa49af503f831ba7a4ff9b8cd1658d
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-04 20:48:01 +0100

    Remove hardcoded path

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.DataFrame
 object Util {
 
   //set this to the home directory of this project
-  val FM1920HOME = "/home/schererc/Schreibtisch/WS1920/Forschungsmodul Datenbanken/forschungsmodul1920"
+  val FM1920HOME = ""
 
   def create_dict_from_nodes(nodes: DataFrame): collection.Map[Long, String] = {
     nodes
