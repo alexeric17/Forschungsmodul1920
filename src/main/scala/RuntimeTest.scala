6d13d04b4da40819dedbcd2033477848989a5ecf
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-16 11:30:58 +0100

    Compute time for each command

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -6,13 +6,13 @@ object RuntimeTest {
     val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
 
     //1 Subgraph with 10K nodes
-    shortest_path_graphx(graph_10k, List(1), 1)
-    shortest_path_pregel(graph_10k, 1)
+    spark.time(shortest_path_graphx(graph_10k, List(1), 1))
+    spark.time(shortest_path_pregel(graph_10k, 1))
     //2 Subgraph with 50K nodes
-    shortest_path_graphx(graph_50k, List(1), 1)
-    shortest_path_pregel(graph_50k, 1)
+    spark.time(shortest_path_graphx(graph_50k, List(1), 1))
+    spark.time(shortest_path_pregel(graph_50k, 1))
     //3 Full graph
-    shortest_path_graphx(graph, List(1), 1)
-    shortest_path_pregel(graph, 1)
+    spark.time(shortest_path_graphx(graph, List(1), 1))
+    spark.time(shortest_path_pregel(graph, 1))
   }
 }
