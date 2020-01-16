a8d0f23e04cf2cf08efea34971d023f3ee107f33
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-16 11:44:49 +0100

    Adapt time computation to distinguish

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -6,13 +6,36 @@ object RuntimeTest {
     val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
 
     //1 Subgraph with 10K nodes
-    spark.time(shortest_path_graphx(graph_10k, List(1), 1))
-    spark.time(shortest_path_pregel(graph_10k, 1))
+    var start = System.nanoTime()
+    shortest_path_graphx(graph_10k, List(1), 1)
+    var end = System.nanoTime()
+    println(s"10K, graphx: ${(end - start) / 1000 / 1000} ms")
+
+    start = System.nanoTime()
+    shortest_path_pregel(graph_10k, 1)
+    end = System.nanoTime()
+    println(s"10K, pregel: ${(end - start) / 1000 / 1000} ms")
+
     //2 Subgraph with 50K nodes
-    spark.time(shortest_path_graphx(graph_50k, List(1), 1))
-    spark.time(shortest_path_pregel(graph_50k, 1))
+    start = System.nanoTime()
+    shortest_path_graphx(graph_50k, List(1), 1)
+    end = System.nanoTime()
+    println(s"50K, graphx: ${(end - start) / 1000 / 1000} ms")
+
+    start = System.nanoTime()
+    shortest_path_pregel(graph_50k, 1)
+    end = System.nanoTime()
+    println(s"50K, pregel: ${(end - start) / 1000 / 1000} ms")
+
     //3 Full graph
-    spark.time(shortest_path_graphx(graph, List(1), 1))
-    spark.time(shortest_path_pregel(graph, 1))
+    start = System.nanoTime()
+    shortest_path_graphx(graph, List(1), 1)
+    end = System.nanoTime()
+    println(s"full, graphx: ${(end - start) / 1000 / 1000} ms")
+
+    start = System.nanoTime()
+    shortest_path_pregel(graph, 1)
+    end = System.nanoTime()
+    println(s"full, pregel: ${(end - start) / 1000 / 1000} ms")
   }
 }
