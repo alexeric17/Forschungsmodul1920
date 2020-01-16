dec31b3eefa298bd93e69e49a0c5aeb0fad6da43
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-16 10:05:04 +0100

    Fix extraction of vertices from graph

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -4,10 +4,10 @@ import org.apache.spark.graphx.VertexId
 object RuntimeTest {
   def main(args: Array[String]): Unit = {
     val graph_10k = get_subgraph(nodesDF, edgeDF, 10000, take_highest = true)
-    val vertices_10k = graph_10k.vertices.collect().toList.map(r => r.asInstanceOf[VertexId])
+    val vertices_10k = graph_10k.vertices.collect().toList.map(r => r._1)
 
     val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
-    val vertices_50k = graph_50k.vertices.collect().toList.map(r => r.asInstanceOf[VertexId])
+    val vertices_50k = graph_50k.vertices.collect().toList.map(r => r._1)
     val vertices_full = graphFrame.vertices.select("id").collect().map(_(0)).toList.map(r => r.asInstanceOf[VertexId])
 
     //1 Subgraph with 10K nodes
