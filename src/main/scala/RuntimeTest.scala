f6d2eb2c4bfa66d711a6c208de084c7aa17c932d
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-16 11:59:30 +0100

    Use id of highy connected node

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -5,36 +5,38 @@ object RuntimeTest {
     val graph_10k = get_subgraph(nodesDF, edgeDF, 10000, take_highest = true)
     val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
 
+    val source_id = 9945
+
     //1 Subgraph with 10K nodes
     var start = System.nanoTime()
-    shortest_path_graphx(graph_10k, List(1), 1)
+    shortest_path_graphx(graph_10k, List(source_id), source_id)
     var end = System.nanoTime()
     println(s"10K, graphx: ${(end - start) / 1000 / 1000} ms")
 
     start = System.nanoTime()
-    shortest_path_pregel(graph_10k, 1)
+    shortest_path_pregel(graph_10k, source_id)
     end = System.nanoTime()
     println(s"10K, pregel: ${(end - start) / 1000 / 1000} ms")
 
     //2 Subgraph with 50K nodes
     start = System.nanoTime()
-    shortest_path_graphx(graph_50k, List(1), 1)
+    shortest_path_graphx(graph_50k, List(source_id), source_id)
     end = System.nanoTime()
     println(s"50K, graphx: ${(end - start) / 1000 / 1000} ms")
 
     start = System.nanoTime()
-    shortest_path_pregel(graph_50k, 1)
+    shortest_path_pregel(graph_50k, source_id)
     end = System.nanoTime()
     println(s"50K, pregel: ${(end - start) / 1000 / 1000} ms")
 
     //3 Full graph
     start = System.nanoTime()
-    shortest_path_graphx(graph, List(1), 1)
+    shortest_path_graphx(graph, List(source_id), source_id)
     end = System.nanoTime()
     println(s"full, graphx: ${(end - start) / 1000 / 1000} ms")
 
     start = System.nanoTime()
-    shortest_path_pregel(graph, 1)
+    shortest_path_pregel(graph, source_id)
     end = System.nanoTime()
     println(s"full, pregel: ${(end - start) / 1000 / 1000} ms")
   }
