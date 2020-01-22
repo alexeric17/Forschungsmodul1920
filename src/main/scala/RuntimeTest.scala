6edc9636c025245269671cd585dc5465d330bac4
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-22 14:06:51 +0100

    Add two more test sizes to RuntimeTest

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -2,21 +2,45 @@ import Util._
 
 object RuntimeTest {
   def main(args: Array[String]): Unit = {
+    val graph_5k = get_subgraph(nodesDF, edgeDF, 5000, take_highest = true)
     val graph_10k = get_subgraph(nodesDF, edgeDF, 10000, take_highest = true)
+    val graph_25k = get_subgraph(nodesDF, edgeDF, 25000, take_highest = true)
     val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
 
     val source_id = 9945
 
-    //1 Subgraph with 10K nodes
+    //Subgraph with 5K nodes
     var start = System.nanoTime()
-    shortest_path_graphx(graph_10k, List(source_id), source_id)
+    shortest_path_graphx(graph_5k, List(source_id), source_id)
     var end = System.nanoTime()
+    println(s"5K, graphx: ${(end - start) / 1000 / 1000} ms")
+
+    start = System.nanoTime()
+    shortest_path_pregel(graph_5k, source_id)
+    end = System.nanoTime()
+    println(s"5K, pregel: ${(end - start) / 1000 / 1000} ms")
+
+    //Subgraph with 10K nodes
+    start = System.nanoTime()
+    shortest_path_graphx(graph_10k, List(source_id), source_id)
+    end = System.nanoTime()
     println(s"10K, graphx: ${(end - start) / 1000 / 1000} ms")
 
     start = System.nanoTime()
     shortest_path_pregel(graph_10k, source_id)
     end = System.nanoTime()
     println(s"10K, pregel: ${(end - start) / 1000 / 1000} ms")
+
+    //Subgraph with 25K nodes
+    start = System.nanoTime()
+    shortest_path_graphx(graph_25k, List(source_id), source_id)
+    end = System.nanoTime()
+    println(s"10K, graphx: ${(end - start) / 1000 / 1000} ms")
+
+    start = System.nanoTime()
+    shortest_path_pregel(graph_25k, source_id)
+    end = System.nanoTime()
+    println(s"25K, pregel: ${(end - start) / 1000 / 1000} ms")
 
     //2 Subgraph with 50K nodes
     start = System.nanoTime()
