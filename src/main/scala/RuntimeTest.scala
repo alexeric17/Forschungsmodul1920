e0a0d43a542499fac76a39f86212b64727417d31
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-23 17:45:57 +0100

    Fix up RuntimeTest
    * simplify sorting of the components
    * add tests for other sizes

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -5,11 +5,11 @@ object RuntimeTest {
 
   def compute_runtime(size: String, components: Array[Graph[String, Double]]): Unit = {
     println(s"$size: #Components: ${components.length}")
-    val sorted_components = components.sortBy(g => g.vertices.collect().length)
+    val sorted_components = components.sortBy(g => -g.vertices.collect().length)
     var runtime_graphx = 0.0
     var runtime_pregel = 0.0
     for (i <- 0 until 10) {
-      val component = sorted_components(sorted_components.length - 1 - i)
+      val component = sorted_components(i)
       println(s"Size of component $i: ${component.vertices.collect().length}")
       val source_id = component.vertices.collect().take(1)(0)._1
       var start = System.nanoTime()
@@ -24,8 +24,10 @@ object RuntimeTest {
       println("Adding " + runtime + " to pregel")
       runtime_pregel += runtime.toFloat
     }
-    println(s"$size, graphx: $runtime_graphx ms")
-    println(s"$size, pregel: $runtime_pregel ms")
+    val avg_runtime_graphx = runtime_graphx / 10
+    val avg_runtime_pregel = runtime_pregel / 10
+    println(s"$size, graphx: $avg_runtime_graphx ms")
+    println(s"$size, pregel: $avg_runtime_pregel ms")
   }
 
   def main(args: Array[String]): Unit = {
@@ -33,62 +35,25 @@ object RuntimeTest {
     val components_5k = subgraphs_from_connected_components(graph_5k)
     val subgraphs_5k = create_all_subgraphs_from_cc(graph_5k, components_5k, components_5k.length)
 
-//    val graph_10k = get_subgraph(nodesDF, edgeDF, 10000, take_highest = true)
-//    val components_10k = subgraphs_from_connected_components(graph_10k)
-//    val subgraphs_10k = create_all_subgraphs_from_cc(graph_10k, components_5k, components_5k.length)
-//
-//    val graph_25k = get_subgraph(nodesDF, edgeDF, 25000, take_highest = true)
-//    val components_25k = subgraphs_from_connected_components(graph_25k)
-//    val subgraphs_25k = create_all_subgraphs_from_cc(graph_25k, components_5k, components_5k.length)
-//
-//    val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
-//    val components_50k = subgraphs_from_connected_components(graph_50k)
-//    val subgraphs_50k = create_all_subgraphs_from_cc(graph_50k, components_5k, components_5k.length)
+    val graph_10k = get_subgraph(nodesDF, edgeDF, 10000, take_highest = true)
+    val components_10k = subgraphs_from_connected_components(graph_10k)
+    val subgraphs_10k = create_all_subgraphs_from_cc(graph_10k, components_10k, components_10k.length)
+
+    val graph_25k = get_subgraph(nodesDF, edgeDF, 25000, take_highest = true)
+    val components_25k = subgraphs_from_connected_components(graph_25k)
+    val subgraphs_25k = create_all_subgraphs_from_cc(graph_25k, components_25k, components_25k.length)
+
+    val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
+    val components_50k = subgraphs_from_connected_components(graph_50k)
+    val subgraphs_50k = create_all_subgraphs_from_cc(graph_50k, components_50k, components_50k.length)
+
+    val components_full = subgraphs_from_connected_components(graph)
+    val subgraphs_full = create_all_subgraphs_from_cc(graph, components_full, components_full.length)
 
     compute_runtime("5K", subgraphs_5k)
-
-//    //Subgraph with 10K nodes
-//    start = System.nanoTime()
-//    shortest_path_graphx(graph_10k, List(source_id), source_id)
-//    end = System.nanoTime()
-//    println(s"10K, graphx: ${(end - start) / 1000 / 1000} ms")
-//
-//    start = System.nanoTime()
-//    shortest_path_pregel(graph_10k, source_id)
-//    end = System.nanoTime()
-//    println(s"10K, pregel: ${(end - start) / 1000 / 1000} ms")
-//
-//    //Subgraph with 25K nodes
-//    start = System.nanoTime()
-//    shortest_path_graphx(graph_25k, List(source_id), source_id)
-//    end = System.nanoTime()
-//    println(s"25k, graphx: ${(end - start) / 1000 / 1000} ms")
-//
-//    start = System.nanoTime()
-//    shortest_path_pregel(graph_25k, source_id)
-//    end = System.nanoTime()
-//    println(s"25K, pregel: ${(end - start) / 1000 / 1000} ms")
-//
-//    //2 Subgraph with 50K nodes
-//    start = System.nanoTime()
-//    shortest_path_graphx(graph_50k, List(source_id), source_id)
-//    end = System.nanoTime()
-//    println(s"50K, graphx: ${(end - start) / 1000 / 1000} ms")
-//
-//    start = System.nanoTime()
-//    shortest_path_pregel(graph_50k, source_id)
-//    end = System.nanoTime()
-//    println(s"50K, pregel: ${(end - start) / 1000 / 1000} ms")
-//
-//    //3 Full graph
-//    start = System.nanoTime()
-//    shortest_path_graphx(graph, List(source_id), source_id)
-//    end = System.nanoTime()
-//    println(s"full, graphx: ${(end - start) / 1000 / 1000} ms")
-//
-//    start = System.nanoTime()
-//    shortest_path_pregel(graph, source_id)
-//    end = System.nanoTime()
-//    println(s"full, pregel: ${(end - start) / 1000 / 1000} ms")
+    compute_runtime("10K", subgraphs_10k)
+    compute_runtime("25K", subgraphs_25k)
+    compute_runtime("50K", subgraphs_50k)
+    compute_runtime("FULL", subgraphs_full)
   }
 }
