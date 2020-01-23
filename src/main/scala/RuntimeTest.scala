4674baec3bccd84398047af6e0559b289c369f01
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-23 09:04:48 +0100

    Test

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -11,18 +11,18 @@ object RuntimeTest {
 //    val graph_25k = get_subgraph(nodesDF, edgeDF, 25000, take_highest = true)
 //    val graph_50k = get_subgraph(nodesDF, edgeDF, 50000, take_highest = true)
 
-    val source_id = 9945
-
-    //Subgraph with 5K nodes
-    var start = System.nanoTime()
-    shortest_path_graphx(graph_5k, List(source_id), source_id)
-    var end = System.nanoTime()
-    println(s"5K, graphx: ${(end - start) / 1000 / 1000} ms")
-
-    start = System.nanoTime()
-    shortest_path_pregel(graph_5k, source_id)
-    end = System.nanoTime()
-    println(s"5K, pregel: ${(end - start) / 1000 / 1000} ms")
+//    val source_id = 9945
+//
+//    //Subgraph with 5K nodes
+//    var start = System.nanoTime()
+//    shortest_path_graphx(graph_5k, List(source_id), source_id)
+//    var end = System.nanoTime()
+//    println(s"5K, graphx: ${(end - start) / 1000 / 1000} ms")
+//
+//    start = System.nanoTime()
+//    shortest_path_pregel(graph_5k, source_id)
+//    end = System.nanoTime()
+//    println(s"5K, pregel: ${(end - start) / 1000 / 1000} ms")
 
 //    //Subgraph with 10K nodes
 //    start = System.nanoTime()
