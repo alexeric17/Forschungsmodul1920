ff1c291f0f38cd131774c1504b4771451ec583a4
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-01-29 13:52:19 +0100

    Save only distinct values
    * set maxIter to 1000 as PageRank was found to be reasonably fast

--- src/main/scala/ComputePageRanks.scala
+++ src/main/scala/ComputePageRanks.scala
@@ -4,9 +4,16 @@ object ComputePageRanks {
   def main(args: Array[String]): Unit = {
     val filtered_graph = get_filtered_graphframe()
     val start = System.nanoTime()
-    val ranks = filtered_graph.pageRank.maxIter(1).run()
+    val ranks = filtered_graph.pageRank
+      .maxIter(1000)
+      .run()
     val runtime = (System.nanoTime() - start) / 1000 / 1000
     print(s"Runtime: $runtime ms\n")
-    ranks.vertices.coalesce(1).write.json(dataDir + "/pageranks")
+    ranks
+      .vertices
+      .distinct()
+      .coalesce(1)
+      .write
+      .json(dataDir + "/pageranks")
   }
 }
