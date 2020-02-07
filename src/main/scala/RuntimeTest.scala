c1dce671327e0fd47a1ebb2bbd1eb5ff5983cb55
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-07 15:26:31 +0100

    Add computation of standard deviation to runtime computation

--- src/main/scala/RuntimeTest.scala
+++ src/main/scala/RuntimeTest.scala
@@ -2,12 +2,14 @@ import java.util.Calendar
 
 import Util._
 
+import scala.collection.mutable.ListBuffer
+
 object RuntimeTest {
   val filtered_graph = get_filtered_graph()
 
   def compute_runtime(nodes: List[Double]): Unit = {
-    var runtime_graphx = 0.0
-    var runtime_pregel = 0.0
+    var runtimes_graphx = new ListBuffer[Double]()
+    var runtimes_pregel = new ListBuffer[Double]()
     val filtered_subgraph = filtered_graph.subgraph(e => nodes.contains(e.srcId) || nodes.contains(e.dstId), (v, _) => nodes.contains(v))
     var size = filtered_subgraph.vertices.collect().length
     val source_id = filtered_subgraph.vertices.collect()(0)._1
@@ -18,18 +20,20 @@ object RuntimeTest {
       shortest_path_graphx(filtered_subgraph, List(source_id), source_id.toInt)
       var runtime = (System.nanoTime() - start) / 1000 / 1000
       println("Adding " + runtime + " ms to graphx")
-      runtime_graphx += runtime.toFloat
+      runtimes_graphx += runtime
 
       start = System.nanoTime()
       shortest_path_pregel(filtered_subgraph, source_id.toInt)
       runtime = (System.nanoTime() - start) / 1000 / 1000
       println("Adding " + runtime + " ms to pregel")
-      runtime_pregel += runtime.toFloat
+      runtimes_pregel += runtime
     }
-    val avg_runtime_graphx = runtime_graphx / 10
-    val avg_runtime_pregel = runtime_pregel / 10
-    println(s"${size / 1000}K, graphx: $avg_runtime_graphx ms")
-    println(s"${size / 1000}K, pregel: $avg_runtime_pregel ms")
+    val avg_runtime_graphx = runtimes_graphx.sum.toLong / runtimes_graphx.length
+    val avg_runtime_pregel = runtimes_pregel.sum.toLong / runtimes_pregel.length
+    val stddev_graphx = math.sqrt(runtimes_graphx.map(a => math.pow(a - avg_runtime_graphx, 2)).sum / runtimes_graphx.size)
+    val stddev_pregel = math.sqrt(runtimes_pregel.map(a => math.pow(a - avg_runtime_pregel, 2)).sum / runtimes_pregel.size)
+    println(s"${size / 1000}K, graphx: $avg_runtime_graphx ms [standard deviation: $stddev_graphx]")
+    println(s"${size / 1000}K, pregel: $avg_runtime_pregel ms [standard deviation: $stddev_pregel]")
   }
 
   def main(args: Array[String]): Unit = {
@@ -43,7 +47,7 @@ object RuntimeTest {
     compute_runtime(biggest_component.take(10000))
     println(s"[${Calendar.getInstance().getTime}] Computing runtime for size 25000")
     compute_runtime(biggest_component.take(25000))
-    println(s"[${Calendar.getInstance().getTime}] Computing runtime for size 5ß000")
+    println(s"[${Calendar.getInstance().getTime}] Computing runtime for size 50000")
     compute_runtime(biggest_component.take(50000))
     println(s"[${Calendar.getInstance().getTime}] Computing runtime for size 150000")
     compute_runtime(biggest_component.take(150000))
