c733ee30624e687ba6bf4892e13af348217b069d
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-08 23:26:51 +0100

    Bugfix

--- src/main/scala/DegreeHeuristicsTest.scala
+++ src/main/scala/DegreeHeuristicsTest.scala
@@ -4,8 +4,8 @@ object DegreeHeuristicsTest {
   def main(args: Array[String]): Unit = {
     val filteredGraph = get_filtered_graph()
     val connected_components = subgraphs_from_connected_components(filteredGraph).sortBy(c => -c.size)
-    val biggest_component = connected_components(0).map(v => v.toDouble).toList
-    val graph_25k = filteredGraph.subgraph(e => biggest_component.take(25000).contains(e.srcId) && biggest_component.contains(e.dstId), (v, _) => biggest_component.contains(v))
+    val biggest_component_25k = connected_components(0).map(v => v.toDouble).toList.take(25000)
+    val graph_25k = filteredGraph.subgraph(e => biggest_component_25k.contains(e.srcId) && biggest_component_25k.contains(e.dstId), (v, _) => biggest_component_25k.contains(v))
     val src_id = graph_25k.vertices.collect()(0)._1.toInt
 
     val ground_truth = shortest_path_pregel(graph_25k, src_id).groupBy(e => e._2._2.length)
