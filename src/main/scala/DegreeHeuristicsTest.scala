ba1e66f713ca9f45f56ba71e2e011d7053bfe05c
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-08 23:19:58 +0100

    Bugfix, Ignore nodes which are not connected to source node

--- src/main/scala/DegreeHeuristicsTest.scala
+++ src/main/scala/DegreeHeuristicsTest.scala
@@ -5,16 +5,18 @@ object DegreeHeuristicsTest {
     val filteredGraph = get_filtered_graph()
     val connected_components = subgraphs_from_connected_components(filteredGraph).sortBy(c => -c.size)
     val biggest_component = connected_components(0).map(v => v.toDouble).toList
-    val graph_25k = filteredGraph.subgraph(e => biggest_component.contains(e.srcId) && biggest_component.contains(e.dstId), (v, _) => biggest_component.contains(v))
+    val graph_25k = filteredGraph.subgraph(e => biggest_component.take(25000).contains(e.srcId) && biggest_component.contains(e.dstId), (v, _) => biggest_component.contains(v))
     val src_id = graph_25k.vertices.collect()(0)._1.toInt
 
     val ground_truth = shortest_path_pregel(graph_25k, src_id).groupBy(e => e._2._2.length)
 
     ground_truth.foreach(chunk => {
-      println("Shortest paths of lengths " + chunk._1 + ":")
-      chunk._2.foreach(v => {
-        println(reProdPath(heuristics_shortestPath_pair(graph_25k, src_id, v._1.toInt, 3)._1, src_id, v._1.toInt).toString())
-      })
+      if (chunk._1 != 0) {
+        println("Shortest paths of lengths " + chunk._1 + ":")
+        chunk._2.foreach(v => {
+          println(reProdPath(heuristics_shortestPath_pair(graph_25k, src_id, v._1.toInt, 3)._1, src_id, v._1.toInt).toString())
+        })
+      }
     })
   }
 }
