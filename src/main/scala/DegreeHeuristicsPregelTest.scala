0936e0d24a0057d9c76809b204e220a1dc410841
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-09 17:49:45 +0100

    Count the actual number of occurences of each error and print them together with the average error for each neighborhood

--- src/main/scala/DegreeHeuristicsPregelTest.scala
+++ src/main/scala/DegreeHeuristicsPregelTest.scala
@@ -1,5 +1,8 @@
 import Util._
 
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object DegreeHeuristicsPregelTest {
   def main(args: Array[String]): Unit = {
     val filteredGraph = get_filtered_graph()
@@ -9,13 +12,14 @@ object DegreeHeuristicsPregelTest {
     val r = scala.util.Random
 
     for (nr_neighbors <- 3 until 10) {
+      var errors = new mutable.HashMap[Int, ListBuffer[Int]]
+
       for (_ <- 0 until 5) {
         val src_id = graph_100k.vertices.collect()(math.abs(r.nextInt() % 100000))._1.toInt //random node
         val ground_truth = shortest_path_pregel(graph_100k, src_id)
           .map(v => (v._1, v._2)).toMap
 
         val interesting_nodes = ground_truth.filter(gt => gt._2._2.nonEmpty)
-        println("Found " + interesting_nodes.toArray.length + " interesting paths!")
 
         val start = System.nanoTime()
         val prediction = heuristic_sssp_pregel(graph_100k, src_id, nr_neighbors)
@@ -24,13 +28,27 @@ object DegreeHeuristicsPregelTest {
 
         var error = 0
         interesting_nodes.foreach(e => {
-          println("Looking at a shortest path of length " + e._2._2.length)
-          val diff = e._2._2.length - prediction_map(e._1)._2.length
-          println("Adding diff of " + diff + " to current error estimate")
-          error += diff
+          val pathlength = e._2._2.length
+          val diff = pathlength - prediction_map(e._1)._2.length
+          if (!errors.contains(pathlength)) {
+            errors(pathlength) = ListBuffer(pathlength)
+          } else {
+            errors(pathlength) += diff
+          }
         })
-        println("Total error of heuristics ("+ nr_neighbors + " neighbors): " + error)
       }
+      println("Neighborhood size: " + nr_neighbors + ", Interesting paths: ")
+      errors.map(e => (e._1, e._2.toList.groupBy(identity).mapValues(_.size))).foreach(pair => {
+        println("Paths of length " + pair._1)
+        var total_nr = 0
+        var total_error = 0
+        pair._2.foreach(v => {
+          println("Difference " + v._1 + ": " + v._2 + " occurences")
+          total_nr += v._2
+          total_error = v._2 * v._1
+        })
+        println("Average error: " + total_error.toFloat / total_nr)
+      })
     }
   }
 }
