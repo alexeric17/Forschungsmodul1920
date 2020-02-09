9ad88c3e6edeb07bc23d423d09da10dd30404d88
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-09 14:20:51 +0100

    Add comparison of results to degree heuristics test

--- src/main/scala/DegreeHeuristicsPregelTest.scala
+++ src/main/scala/DegreeHeuristicsPregelTest.scala
@@ -6,19 +6,27 @@ object DegreeHeuristicsPregelTest {
     val connected_components = subgraphs_from_connected_components(filteredGraph).sortBy(c => -c.size)
     val biggest_component_100k = connected_components(0).map(v => v.toDouble).toList.take(100000)
     val graph_100k = filteredGraph.subgraph(e => biggest_component_100k.contains(e.srcId) && biggest_component_100k.contains(e.dstId), (v, _) => biggest_component_100k.contains(v))
-    val src_id = graph_100k.vertices.collect()(0)._1.toInt
+    val r = scala.util.Random
 
-    val ground_truth = shortest_path_pregel(graph_100k, src_id).groupBy(e => e._2._2.length)
+    for (nr_neighbors <- 3 until 10) {
+      for (i <- 0 until 5) {
+        val src_id = graph_100k.vertices.collect()(math.abs(r.nextInt() % 100000))._1.toInt //random node
+        val ground_truth = shortest_path_pregel(graph_100k, src_id)
+          .map(v => (v._1, v._2)).toMap
 
-    for (i <- 3 until 10) {
-      ground_truth.foreach(chunk => {
-        if (chunk._1 != 0) {
-          println("Shortest paths of lengths " + chunk._1 + ":")
-          val start = System.nanoTime()
-          val annotated_graph = heuristic_sssp_pregel(graph_100k, src_id, i)
-          println("Runtime ("+ i + " neighbors): " + (System.nanoTime() - start) / 1000 / 1000 + "ms")
-        }
-      })
+        val start = System.nanoTime()
+        val prediction = heuristic_sssp_pregel(graph_100k, src_id, i)
+        println("Heuristics Runtime ("+ i + " neighbors): " + (System.nanoTime() - start) / 1000 / 1000 + "ms")
+        val prediction_map = prediction.map(v => (v._1, v._2)).toMap
+
+        var error = 0
+        ground_truth.foreach(e => {
+          val diff = e._2._2.length - prediction_map(e._1)._2.length
+          println("Adding diff of " + diff + " to current error estimate")
+          error += diff
+        })
+        println("Total error of heuristics ("+ i + " neighbors): " + error)
+      }
     }
   }
 }
