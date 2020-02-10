4ffadb67430727cb85108ee44edc0c2751139e89
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-10 21:02:41 +0100

    Debug print

--- src/main/scala/ComputeDegreeCore.scala
+++ src/main/scala/ComputeDegreeCore.scala
@@ -10,6 +10,8 @@ object ComputeDegreeCore {
   def main(args: Array[String]): Unit = {
     val filtered_graph = get_filtered_graph()
     val result = mutable.Map[(VertexId, VertexId), List[VertexId]]()
+    var nr_found_paths = 0
+    var nr_total_paths = 0
 
     println(s"[${Calendar.getInstance().getTime}] Computing In Degrees for Degree Heuristics Core")
 
@@ -25,7 +27,7 @@ object ComputeDegreeCore {
       .filter(v => g_degIn(v._1) != 0)
       .sortBy(v => -v._2).take(100)
 
-    println("Computing the core for the following vertices:")
+    println(s"Computing the core on graph of size ${filtered_graph.vertices.collect().length} for the following vertices:")
     core_nodes.foreach(v => println(s"ID ${v._1} with degree ${v._2} (In-degree: ${g_degIn(v._1)})"))
 
     core_nodes.foreach(dst => {
@@ -33,9 +35,14 @@ object ComputeDegreeCore {
       val paths = shortest_path_pregel(filtered_graph, dst._1.toInt)
       println(s"Done computing after ${(System.nanoTime() - start) / 1000 / 1000} ms")
       paths.foreach(src => {
+        nr_total_paths += 1
+        if (src._2._2.nonEmpty) {
+          nr_found_paths += 1
+        }
         result((src._1, dst._1)) = src._2._2
       })
     })
+    println(s"Percentage of found paths: ${nr_found_paths.toDouble / nr_total_paths}")
     result.toSeq.toDF("pair", "shortest_path").coalesce(1).write.json(dataDir + "/core_degree")
   }
 }
