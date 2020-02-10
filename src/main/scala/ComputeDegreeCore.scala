a72cb0040be4e6efd64db0ed0417ed58bde0297b
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-10 20:07:11 +0100

    Compute shortest paths and save them to json

--- src/main/scala/ComputeDegreeCore.scala
+++ src/main/scala/ComputeDegreeCore.scala
@@ -1,16 +1,31 @@
+import java.util.Calendar
+
 import Util._
+import org.apache.spark.graphx.lib.ShortestPaths
 
 object ComputeDegreeCore {
   def main(args: Array[String]): Unit = {
     val filtered_graph = get_filtered_graph()
-    val connected_components = subgraphs_from_connected_components(filtered_graph).sortBy(c => -c.size)
-    val biggest_component = connected_components(0).map(v => v.toDouble).toList
-    val filtered_subgraph = filtered_graph.subgraph(e =>
-      biggest_component.contains(e.srcId) || biggest_component.contains(e.dstId), (v, _) => biggest_component.contains(v))
 
-    val g_degOut = filtered_subgraph.outerJoinVertices(filtered_subgraph.outDegrees)((_, _, deg) => deg.getOrElse(0))
-    val core_nodes = g_degOut.vertices.sortBy(v => -v._2).take(100)
+    println(s"[${Calendar.getInstance().getTime}] Computing In Degrees for Degree Heuristics Core")
+
+    val g_degIn = filtered_graph.outerJoinVertices(filtered_graph.inDegrees)((_, _, deg) => deg.getOrElse(0))
+      .vertices.collectAsMap()
+
+    println(s"[${Calendar.getInstance().getTime}] Computing Out Degrees for Degree Heuristics Core")
+
+    val g_degOut = filtered_graph.outerJoinVertices(filtered_graph.outDegrees)((_, _, deg) => deg.getOrElse(0))
+      .vertices
+
+    val core_nodes = g_degOut
+      .filter(v => g_degIn(v._1) != 0)
+      .sortBy(v => -v._2).take(100)
 
     core_nodes.foreach(v => println(s"ID ${v._1} with degree ${v._2}"))
+
+    val result = ShortestPaths.run(filtered_graph, core_nodes.map(v => v._1))
+
+    spark.createDataFrame(result.vertices.distinct()).toDF().coalesce(1).write.json(FM1920HOME + "/data/core_degree")
+
   }
 }
