c2fd06b963adc672078640577850d7c1f743aa17
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-10 20:50:22 +0100

    Save the results in json

--- src/main/scala/ComputeDegreeCore.scala
+++ src/main/scala/ComputeDegreeCore.scala
@@ -1,11 +1,15 @@
 import java.util.Calendar
 
 import Util._
-import org.apache.spark.graphx.lib.ShortestPaths
+import org.apache.spark.graphx.VertexId
+
+import scala.collection.mutable
+import spark.implicits._
 
 object ComputeDegreeCore {
   def main(args: Array[String]): Unit = {
     val filtered_graph = get_filtered_graph()
+    val result = mutable.Map[(VertexId, VertexId), List[VertexId]]()
 
     println(s"[${Calendar.getInstance().getTime}] Computing In Degrees for Degree Heuristics Core")
 
@@ -22,12 +26,16 @@ object ComputeDegreeCore {
       .sortBy(v => -v._2).take(100)
 
     println("Computing the core for the following vertices:")
-    core_nodes.foreach(v => println(s"ID ${v._1} with degree ${v._2} (In-degree: ${g_degIn(v._1)}"))
+    core_nodes.foreach(v => println(s"ID ${v._1} with degree ${v._2} (In-degree: ${g_degIn(v._1)})"))
 
-    val start = System.nanoTime()
-    val result = ShortestPaths.run(filtered_graph, core_nodes.map(v => v._1))
-    println(s"Done computing after ${(System.nanoTime() - start) / 1000 / 1000} ms")
-
-    spark.createDataFrame(result.vertices.distinct()).toDF().coalesce(1).write.json(FM1920HOME + "/data/core_degree")
+    core_nodes.foreach(dst => {
+      val start = System.nanoTime()
+      val paths = shortest_path_pregel(filtered_graph, dst._1.toInt)
+      println(s"Done computing after ${(System.nanoTime() - start) / 1000 / 1000} ms")
+      paths.foreach(src => {
+        result((src._1, dst._1)) = src._2._2
+      })
+    })
+    result.toSeq.toDF("pair", "shortest_path").write.json(dataDir + "/core_degree")
   }
 }
