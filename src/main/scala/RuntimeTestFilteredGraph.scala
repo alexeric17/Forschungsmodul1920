c139b5591fb4a74474585008c179ad1d00944f90
Christian Johannes Scherer <christian.scherer@student.uni-augsburg.de>
2020-02-11 23:05:38 +0100

    Compute for graphx-standard and our pregel

--- src/main/scala/RuntimeTestFilteredGraph.scala
+++ src/main/scala/RuntimeTestFilteredGraph.scala
@@ -2,14 +2,12 @@ import java.util.Calendar
 
 import Util._
 
-import scala.collection.mutable
-
 object RuntimeTestFilteredGraph {
   val filtered_graph = get_filtered_graph()
 
   def compute_runtime(): Unit = {
+    var runtime_graphx = 0.0
     var runtime_pregel = 0.0
-    var pathlengths = new mutable.HashMap[Int, Int]
     var size = filtered_graph.vertices.collect().length
     val r = scala.util.Random
 
@@ -17,26 +15,25 @@ object RuntimeTestFilteredGraph {
     for (i <- 0 until 30) {
       val source_id = filtered_graph.vertices.collect()(math.abs(r.nextInt() % size))._1 //random node
       println("Computing SSSP for vertex with id " + source_id)
-      val start = System.nanoTime()
-      val vertices = shortest_path_pregel(filtered_graph, source_id.toInt)
-      val runtime = (System.nanoTime() - start) / 1000 / 1000
+
+      var start = System.nanoTime()
+      shortest_path_graphx(filtered_graph, List(source_id), source_id.toInt)
+      var runtime = (System.nanoTime() - start) / 1000 / 1000
+      println("Adding " + runtime + " ms to graphx")
+      runtime_graphx += runtime.toFloat
+
+      start = System.nanoTime()
+      shortest_path_pregel(filtered_graph, source_id.toInt)
+      runtime = (System.nanoTime() - start) / 1000 / 1000
       println("Adding " + runtime + " ms to pregel")
       runtime_pregel += runtime.toFloat
 
-      vertices.foreach(v => {
-        val length = v._2._2.length
-        if (!pathlengths.contains(length)) {
-          pathlengths(length) = 1
-        } else {
-          pathlengths(length) += 1
-        }
-      })
-      println("Current average path lengths: ")
-      pathlengths.foreach(p => println("Nr of paths of Length " + p._1 + ": " + p._2))
     }
     val avg_runtime_pregel = runtime_pregel / 30
+    val avg_runtime_graphx = runtime_graphx / 30
+
+    println(s"${size / 1000}K, graphx: $avg_runtime_graphx ms")
     println(s"${size / 1000}K, pregel: $avg_runtime_pregel ms")
-    pathlengths.foreach(p => println("Nr of paths of Length " + p._1 + ": " + p._2))
 
   }
 
