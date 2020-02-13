d2aa1929f7f21e608e5a2d8a7a8c743535798c7f
ericalex <ericalex@student.uni-augsburg.de>
2020-02-13 12:58:19 +0100

    fix to save proper graph without multiple edges.

--- src/main/scala/SaveFilteredGraph.scala
+++ src/main/scala/SaveFilteredGraph.scala
@@ -3,7 +3,7 @@ import Util._
 object SaveFilteredGraph {
   def main(args: Array[String]): Unit = {
     val filtered_graph = compute_filtered_graphframe()
-    filtered_graph.vertices.coalesce(1).distinct().write.json(filteredNodeDir)
-    filtered_graph.edges.coalesce(1).distinct().write.json(filteredEdgeDir)
+    filtered_graph.vertices.distinct().coalesce(1).write.json(filteredNodeDir)
+    filtered_graph.edges.distinct().coalesce(1).write.json(filteredEdgeDir)
   }
 }
