0adf08fc21f1e0cd47348891a01cd5cfff6b40e7
ericalex <ericalex@student.uni-augsburg.de>
2020-02-19 14:25:11 +0100

    remove the sortBy step and filter step during iterations.

--- src/main/scala/DegreeCentrality.scala
+++ src/main/scala/DegreeCentrality.scala
@@ -46,16 +46,18 @@ object DegreeCentrality {
     //For all random nodes in subGraph.
     for(next <- randomNodes){
       //Make sure we keep only nodes from the connected component at each iteration.
-      val sssp1 = most_seen_vertex_sssp_pregel(filtered_graph,next).filter(x => subGraphVerticies.contains(x._1))
+      val sssp1 = most_seen_vertex_sssp_pregel(filtered_graph,next)
       //Update result.
       sssp1.foreach(node => result.update(node._1,result.getOrElse(node._1,0) + node._2))
-      if((i > 300) && (i%50 == 0)) {
+      println(s"Iteration $i\n")
+      if((i >= 50) && (i%50 == 0)) {
         val sortedResult = result.toSeq
         println(s"50 nodes at iteration: $i " ,sortedResult.take(50).mkString("\n"))
         spark.sparkContext.parallelize(sortedResult).coalesce(1).saveAsTextFile(path + s"$i")
       }
       i += 1
     }
+    spark.sparkContext.parallelize(result.toSeq.filter(x => subGraphVerticies.contains(x._1))).coalesce(1)
 
     /*
     //TODO create a immutable map which can be sorted. BELOW.
